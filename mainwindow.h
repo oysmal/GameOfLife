0790b6a0451046a04900ca66959d71e21148146d
Øystein Malt <oysmal@gmail.com>
2015-03-09 13:14:32 +0100

    removing files from version control

--- mainwindow.h
+++ mainwindow.h
@@ -3,6 +3,11 @@
 
 #include <QMainWindow>
 #include "eventinterface.h"
+#include "grid.h"
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QGraphicsRectItem>
+#include <QList>
 
 namespace Ui {
 class MainWindow;
@@ -15,9 +20,15 @@ class MainWindow : public QMainWindow, public EventInterface
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+    void loadGridIntoView();
+    void render();
 
 private:
     Ui::MainWindow *ui;
+    Grid *grid;
+    QGraphicsScene *scene;
+    QList<QGraphicsRectItem*> points;
+    QGraphicsView graphicsView;
 
 private slots:
     void sendEventTest();
